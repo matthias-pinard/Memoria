81c4e4968b801069921c3ed00d6532df9de73b48
Matthias <matthiaspinard@gmail.com>
2019-02-10 23:59:47 +0100

    change image

--- src/utils/getPeople.js
+++ src/utils/getPeople.js
@@ -16,22 +16,51 @@ let getPhoneNumber = () => {
 
 let getFace = () => {
   let i = Math.floor(Math.random() * 200);
-  return require("./img/" + i + ".jpg");
+  return require("./img/" + i + ".png");
 };
 
-let getPeople = () => {
-  return {
-    face: getFace(),
-    name: getName(),
-    num: getPhoneNumber()
-  };
-};
-
-export default nb => {
+let getPhoneNumbers = i => {
   let arr = [];
-  let i;
-  for (i = 0; i < nb; i++) {
-    arr.push(getPeople(i));
+  while (arr.length < i) {
+    let num = getPhoneNumber();
+    if (!arr.includes(num)) {
+      arr.push(num);
+    }
   }
   return arr;
 };
+
+let getNames = i => {
+  let arr = [];
+  while (arr.length < i) {
+    let name = getName();
+    if (!arr.includes(name)) {
+      arr.push(name);
+    }
+  }
+  return arr;
+};
+
+let getFaces = i => {
+  let arr = [];
+  while (arr.length < i) {
+    let face = getFace();
+    if (!arr.includes(face)) {
+      arr.push(face);
+    }
+  }
+  return arr;
+};
+
+let getPeoples = nbPeoples => {
+  let faces = getFaces(nbPeoples);
+  let names = getNames(nbPeoples);
+  let nums = getPhoneNumbers(nbPeoples);
+  let peoples = [];
+  for (let i = 0; i < nbPeoples; i++) {
+    peoples.push({ face: faces[i], name: names[i], num: nums[i] });
+  }
+  return peoples;
+};
+
+export default i => getPeoples(i);
