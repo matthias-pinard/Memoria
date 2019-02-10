3dbee724678fcf239e7b19ba38f2b0757fad628b
Matthias <matthiaspinard@gmail.com>
2019-02-11 00:16:40 +0100

    convert all image to jpg

--- src/utils/getPeople.js
+++ src/utils/getPeople.js
@@ -16,7 +16,7 @@ let getPhoneNumber = () => {
 
 let getFace = () => {
   let i = Math.floor(Math.random() * 200);
-  return require("./img/" + i + ".png");
+  return require("./img/" + i + ".jpg");
 };
 
 let getPhoneNumbers = i => {
